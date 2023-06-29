a2312504ab00b2537a72965cfc1b4e9d030fd0bc
ngumbk <chulkov.nikita01@gmail.com>
2023-06-29 21:10:07 +0300

    Server's POST returns 10 last messages

--- server.py
+++ server.py
@@ -11,16 +11,22 @@ class Message(BaseModel):
 
 app = FastAPI()
 
+dbconfig = {
+  "database": "server_db",
+  "user": "root",
+  "password": "123456"
+}
+
 try:
-    mydb = mysql.connector.connect(
+    init_cnx = mysql.connector.connect(
         host='localhost',
         user='root',
         password='123456'
     )
-    cursor = mydb.cursor()
+    cursor = init_cnx.cursor()
     cursor.execute("SHOW DATABASES LIKE 'server_db'")
     if cursor.fetchone() == None:
-        # create db
+        # Create DB in case one doesn't exist
         cursor.execute("CREATE DATABASE server_db")
         cursor.execute("USE server_db")
         cursor.execute("CREATE TABLE Messages ("
@@ -31,7 +37,9 @@ try:
                         "PRIMARY KEY (message_id));")
         print('DB Created!')
     cursor.close()
-    
+    cnxpool = mysql.connector.pooling.MySQLConnectionPool(pool_name="mypool",
+                                                          pool_size=3,
+                                                          **dbconfig)
 except mysql.connector.Error as err:
     print(err)
 
@@ -39,11 +47,36 @@ except mysql.connector.Error as err:
 # Returns DB connection
 @app.get("/")
 def read_root():
-    return {"Hello": "World",
-            "mydb": str(mydb)}
+    return {"Hello": "World"}
 
 
 @app.post("/send_message/")
 async def send_message(message: Message):
     message.time = time.strftime('%Y-%m-%d %H:%M:%S')
-    return {"sender_name": message.name, "message_text": message.text, "message_time": message.time}
+    cnx1 = cnxpool.get_connection()
+    cursor = cnx1.cursor()
+    cursor.execute("USE server_db")
+    cursor.execute("INSERT INTO Messages(sender_name, message_text, message_time) "
+                   f"VALUES ('{message.name}', '{message.text}', '{message.time}')")
+    cnx1.commit()
+    cursor.close()
+    cnx1.close()
+
+    # Getting last 10 messages
+    cnx2 = cnxpool.get_connection()
+    cursor = cnx2.cursor()
+    cursor.execute("SELECT COUNT(*) FROM Messages")
+    entries_count = cursor.fetchone()[0]
+    cursor.execute("SELECT * FROM Messages ORDER BY message_id DESC LIMIT 10")
+    return_dict = {}
+    if entries_count < 10:
+        for i in range(entries_count):
+            return_dict[i] = cursor.fetchone()
+    else:
+        for i in range(10):
+            return_dict[i] = cursor.fetchone()
+    print(*return_dict.items(), sep='\n')
+    reversed_dict = {}
+    for i in range(10):
+        reversed_dict[i] = return_dict[9 - i]
+    return reversed_dict
